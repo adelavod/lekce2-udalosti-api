5d29726e93a71a6cecbc3e3c2665a959425401a2
Adéla Krásová <>
2022-02-15 19:06:33 +0100

    classList usage

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 // tady je místo pro náš program
+console.log("ahoyyy");
+
+let nadpis = document.querySelector("h1");
+nadpis.classList.add("zluty");
+nadpis.classList.remove("zluty"); // dali jsme to tam a zas odebrali
+// jeste TOGGLE
+
+let ctverecek = document.querySelector("div.zluty");
+ ctverecek.classList.add("ctverecek");
+
+ let pocitadlo = 0;
+ function priNajeti () {
+     console.log("test " + pocitadlo);
+
+     pocitadlo = pocitadlo + 1;
+ };
+
+ function zmenStyl(){
+     nadpis.classList.toggle("zeleny");
+ }
